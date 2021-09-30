c70f70fffd36b6fb27423e715e0319ae429ca98a
Naikey Emilcar <naikeybe@Knights.ucf.edu>
2021-09-30 15:43:44 -0400

    pseudo-code

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,36 @@
+package baseline;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Naikey Emilcar
+ */
+
+public class Solution24 {
+
+    public static boolean isAnagram(String word1, String word2){
+
+        //compare the two received strings
+
+        //return true if both strings are anagram false otherwise
+
+        return false;
+    }
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        //Greet user with app purpose
+
+        //Ask for first string
+
+        //Ask for second string
+
+        //Call anagram checker method
+
+        //display result of anagram checker
+
+    }
+
+}
