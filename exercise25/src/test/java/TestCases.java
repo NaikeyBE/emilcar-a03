9466ee93ae21e30be96491a57fcabe8e0bde5e59
Naikey Emilcar <naikeybe@Knights.ucf.edu>
2021-10-03 09:41:37 -0400

    updated comments, added logic

--- exercise25/src/test/java/TestCases.java
+++ exercise25/src/test/java/TestCases.java
@@ -1,2 +1,65 @@
-public class TestCases {
+import org.junit.jupiter.api.Test;
+
+import static baseline.Solution25.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestCases {
+
+    @Test
+    void testLengthChecker(){
+
+        String password = "123456789";
+
+        boolean expectedLength = true;
+        boolean actualLength = lengthChecker(password);
+
+        assertEquals(expectedLength, actualLength);
+
+    }
+
+    @Test
+    void testNumberChecker(){
+
+        String password = "12n45";
+
+        boolean expectedCheck = false;
+        boolean actualCheck = numberChecker(password);
+
+        assertEquals(expectedCheck, actualCheck);
+
+    }
+
+    @Test
+    void testLetterChecker(){
+
+        String password = "12n45";
+
+        boolean expectedCheck = false;
+        boolean actualCheck = letterChecker(password);
+
+        assertEquals(expectedCheck, actualCheck);
+
+    }
+
+    @Test
+    void testNumberAndLetterChecker() {
+
+        String password = "1234576789a";
+
+        boolean expectedCheck = true;
+        boolean actualCheck = letterAndNumberChecker(password);
+
+        assertEquals(expectedCheck, actualCheck);
+    }
+
+    @Test
+    void testSpecialChecker() {
+
+        String password = "1%2345";
+
+        boolean expectedCheck = true;
+        boolean actualCheck = specialChecker(password);
+
+        assertEquals(expectedCheck, actualCheck);
+    }
 }
